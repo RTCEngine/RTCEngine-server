408cd3039e29e18bf481b8b02a7c6f2e0974f149
notedit <notedit@gmail.com>
2019-06-05 14:44:57 +0800

    add args parser

--- media01.ts
+++ media01.ts
@@ -4,9 +4,35 @@ import MediaServer from './medianode/server'
 
 config()
 
-const port = 6001
-const host = process.env.HOST ? process.env.HOST : '127.0.0.1'
-const endpoint = process.env.ENDPOINT ? process.env.ENDPOINT : '127.0.0.1'
+const yargs = require('yargs')
+
+
+const argv = yargs.usage('Usage: ts-node $0 -p 6001 -h 127.0.0.1 -e 127.0.0.1')
+    .help('help').alias('help','-h')
+    .default({
+        endpoint:'127.0.0.1',
+        host:'127.0.0.1',
+        port:6001
+    })
+    .options({
+        endpoint: {
+            alias: 'e',
+            description: 'endpoint, default 127.0.0.1'
+        },
+        host: {
+            alias: 'h',
+            description: 'host, default 127.0.0.1'
+        },
+        port: {
+            alias: 'p',
+            description: 'port, default 6001'
+        }
+    }).argv
+
+
+const port = argv.port as number
+const host = argv.host as string
+const endpoint = argv.endpoint as string 
 
 const media01 = new MediaServer({
     endpoint: endpoint
