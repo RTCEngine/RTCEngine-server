884e2232b5a95abe1033a3e9839c7c4caea74240
notedit <notedit@gmail.com>
2019-06-06 17:47:05 +0800

    fix config

--- medianode/config.ts
+++ medianode/config.ts
@@ -11,11 +11,11 @@ export default {
         audio: {
             codecs: ['opus'],
             extensions: [
-                'http://www.ietf.org/id/draft-holmer-rmcat-transport-wide-cc-extensions-01',
                 'urn:ietf:params:rtp-hdrext:ssrc-audio-level',
                 'urn:ietf:params:rtp-hdrext:sdes:mid',
                 'urn:ietf:params:rtp-hdrext:sdes:rtp-stream-id',
-                'urn:ietf:params:rtp-hdrext:sdes:repaired-rtp-stream-id'
+                'urn:ietf:params:rtp-hdrext:sdes:repaired-rtp-stream-id',
+                'http://www.ietf.org/id/draft-holmer-rmcat-transport-wide-cc-extensions-01'
             ]
         },
         video: {
@@ -31,12 +31,12 @@ export default {
             ],
             extensions: [
                 'urn:3gpp:video-orientation',
-                'http://www.webrtc.org/experiments/rtp-hdrext/abs-send-time',
-                'http://www.ietf.org/id/draft-holmer-rmcat-transport-wide-cc-extensions-01',
-                'http://www.webrtc.org/experiments/rtp-hdrext/playout-delay',
                 'urn:ietf:params:rtp-hdrext:sdes:mid',
                 'urn:ietf:params:rtp-hdrext:sdes:rtp-stream-id',
-                'urn:ietf:params:rtp-hdrext:sdes:repaired-rtp-stream-id'
+                'urn:ietf:params:rtp-hdrext:sdes:repaired-rtp-stream-id',
+                'http://www.webrtc.org/experiments/rtp-hdrext/abs-send-time',
+                'http://www.ietf.org/id/draft-holmer-rmcat-transport-wide-cc-extensions-01',
+                'http://www.webrtc.org/experiments/rtp-hdrext/playout-delay'
             ]
         }
     }
